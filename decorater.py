fd91cae126b021ec0dfe156a91e65f4b0f7afa29
MarvinLiang <961239951@qq.com>
2020-07-23 14:10:49 +0800

    Update decorater.py

--- decorater.py
+++ decorater.py
@@ -7,7 +7,26 @@ def login(func):
         return func(*args,**kargs)
     return wrapper
 
+'''自身传入参数的装饰器（采用三层函数定义装饰器）'''
+def login(text):
+    def decorator(func):
+        def wrapper(*args,**kargs):
+            print('function name %s'%func.__name__)
+            return func(*args,**kargs)
+        return wrapper
+    return decorator
+# 等价于 -> (login(text))(f) -> return wrapper
 
+'''类中的装饰器 @classmethod @staticmethod @property
+普通方法：对象.method(self)
+@classmethod： 类名.class_method(cls), 对象.class_method(cls)
+@staticmethod： 类名.static_method(), 对象.static_method()
+
+使用场景：
+不需要用到与类相关的属性与方法时，就用静态方法@staticmethod
+需要用到与类相关的属性或方法，又想表明这个方法是整个类通用的，而不是对象特异的，使用类方法@classmethod
+
+'''
 
 def fn_timer(function):
     '''
